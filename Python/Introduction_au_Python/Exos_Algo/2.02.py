517800500e84724412ad760ba494de9fb2c8150c
Mohamad Al Khatib <mohamad@juno.tech>
2022-10-25 17:21:51 +0200

    add env vir to fastapi

--- Python/Introduction_au_Python/Exos_Algo/2.02.py
+++
@@ -1,9 +0,0 @@
-number = int(input('Please write a number: '))
-result = 0
-
-for value in range(number + 1):
-    result += value
-
-print (result)    
-
-# Complexité linéare O(n) puisque on a qu'une seule boucle 
--- Python/Introduction_au_Python/Exos_Algo/2.03.py
+++
@@ -1,9 +0,0 @@
-numbers = [ 1, 2, 3, 4, 5 ]
-result = 0
-
-for value in numbers:
-    result += value
-
-print (result)    
-
-# Complexité linéare O(n) puisque on a qu'une seule boucle 
