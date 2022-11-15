166b4d17031bc2c21ead3ebb6505d6789c8fd3fa
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2022-11-15 09:57:52 +0100

    exo 2

--- M1/React/jour2/src/components/Exo2.jsx
+++ M1/React/jour2/src/components/Exo2.jsx
@@ -1,18 +1,27 @@
 const Exo2 = () => {
-	const etudiants = [
-		{ id: 1, nom: "Alain", isStagaire: true },
-		{ id: 2, nom: "Benoit", isStagaire: false },
-		{ id: 3, nom: "Céline", isStagaire: true },
-		{ id: 4, nom: "Damien", isStagaire: true },
+	const articles = [
+		{ id: 1, titre: "article 1", publie: true },
+		{ id: 2, titre: "article 2", publie: false },
+		{ id: 3, titre: "article 3", publie: false },
+		{ id: 4, titre: "article 4", publie: true },
 	];
 
+	const mouseOverHanlder = (article) => {
+		console.log(article.titre);
+	};
 	return (
 		<div className="container">
+			<h1 className="h2 mb-3">Exo 1</h1>
 			<ul>
-				{etudiants.length >= 1 &&
-					etudiants.map((etudiant) => {
-						return <li key={etudiant.id}>{etudiant.nom}</li>;
-					})}
+				{articles.map((article) => {
+					return (
+						!article.publie && (
+							<li onMouseOver={() => mouseOverHanlder(article)} key={article.id}>
+								{article.titre}
+							</li>
+						)
+					);
+				})}
 			</ul>
 		</div>
 	);
