68b9cdef23b639941ec0e1f662979bbe997e4963
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2022-11-16 10:25:05 +0100

    React, jour3 - active nav link

--- M1/React/jour3/src/components/Global/Menu.jsx
+++ M1/React/jour3/src/components/Global/Menu.jsx
@@ -1,33 +1,37 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 const Menu = () => {
+	const activeLinkClass = {
+		color: "#fff",
+	};
+
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
 			<div className="container">
-				<Link to="/" className="navbar-brand">
+				<NavLink to="/" className="navbar-brand">
 					<img src={logo} alt="logo" />
-				</Link>
+				</NavLink>
 				<button className="navbar-toggler" type="button">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<Link to="/" className="nav-link active">
+							<NavLink to="/" className="nav-link" style={({ isActive }) => (isActive ? activeLinkClass : undefined)}>
 								Home
-							</Link>
+							</NavLink>
 						</li>
 
 						<li className="nav-item">
-							<Link to="/contact" className="nav-link active">
+							<NavLink to="/contact" className="nav-link" style={({ isActive }) => (isActive ? activeLinkClass : undefined)}>
 								Contact
-							</Link>
+							</NavLink>
 						</li>
 
 						<li className="nav-item">
-							<Link to="/a-propos" className="nav-link active">
+							<NavLink to="/a-propos" className="nav-link" style={({ isActive }) => (isActive ? activeLinkClass : undefined)}>
 								A propos
-							</Link>
+							</NavLink>
 						</li>
 					</ul>
 				</div>
