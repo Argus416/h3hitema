d7773047c58ef6f2fba1ca7f65544c7989e7f15b
Mohamad Al Khatib <alkhatib.m804@gmail.com>
2022-11-17 20:11:25 +0100

    create calendar components

--- M1/Projets/react-calendar/src/Helpers/Date.js
+++ M1/Projets/react-calendar/src/Helpers/Date.js
@@ -1,12 +1,12 @@
 export const getDaysInMonth = (month, year) =>
     new Array(31)
-    .fill("")
+    .fill(null)
     .map((v, i) => new Date(year, month - 1, i + 1))
     .filter((v) => v.getMonth() === month - 1)
     .map((day) => day.getDate());
 
 export const getDaysInMonthUTC = (month, year) =>
     new Array(31)
-    .fill("")
+    .fill(null)
     .map((v, i) => new Date(year, month - 1, i + 1))
     .filter((v) => v.getMonth() === month - 1);
--- M1/Projets/react-calendar/src/pages/Home.jsx
+++ M1/Projets/react-calendar/src/pages/Home.jsx
@@ -1,60 +1,10 @@
 import { Box, Button, Container } from "@mui/material";
-import { useState } from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import { getDaysInMonth, getDaysInMonthUTC } from "../Helpers/Date";
+import Calendar from "../components/Calendar";
 
 const Home = () => {
-	const date = new Date();
-	const [year, setYear] = useState(date.getFullYear());
-	const [month, setMonth] = useState(date.getMonth() + 1);
-	const [days, setDays] = useState([]);
-
-	const monthInc = () => {
-		if (month === 12) {
-			setYear(year + 1);
-			setMonth(1);
-		} else {
-			setMonth(month + 1);
-		}
-	};
-
-	const monthDes = () => {
-		if (month === 1) {
-			setYear(year - 1);
-			setMonth(12);
-		} else {
-			setMonth(month - 1);
-		}
-	};
-
-	let allDays = getDaysInMonth(month, year);
-	allDays = allDays.map((day) => day + " ").join();
-	console.log(allDays, "allDays");
-
 	return (
 		<Container>
-			<Box sx={{ marginTop: 5, display: "flex", alignItems: "center", gap: 2 }}>
-				<Button variant="contained" onClick={() => setYear(year - 1)}>
-					<ArrowBackIcon />
-				</Button>
-				{year}
-				<Button variant="contained" onClick={() => setYear(year + 1)}>
-					<ArrowForwardIcon />
-				</Button>
-			</Box>
-
-			<Box sx={{ marginTop: 5, display: "flex", alignItems: "center", gap: 2 }}>
-				<Button variant="contained" onClick={monthDes}>
-					<ArrowBackIcon />
-				</Button>
-				{month}
-				<Button variant="contained" onClick={monthInc}>
-					<ArrowForwardIcon />
-				</Button>
-			</Box>
-
-			<Box sx={{ marginTop: 5, display: "flex", alignItems: "center", gap: 2 }}>{allDays}</Box>
+			<Calendar />
 		</Container>
 	);
 };
