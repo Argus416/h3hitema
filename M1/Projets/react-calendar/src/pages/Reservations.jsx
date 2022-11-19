e12c788e8d9d85733f4c65d1ba06f301cdc928d6
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2022-11-19 13:15:46 +0100

    style navbar, create reservation page

---
+++ M1/Projets/react-calendar/src/pages/Reservations.jsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+
+const Reservations = () => {
+	return (
+		<Container sx={{ marginTop: 3 }}>
+			<Typography variant="h3" color="initial">
+				Reservations
+			</Typography>
+		</Container>
+	);
+};
+
+export default Reservations;
