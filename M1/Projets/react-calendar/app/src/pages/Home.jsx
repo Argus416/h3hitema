150313f9e87c6fe0ccca72006d76e23d28b995cb
Mohamad Al Khatib <mohamad@juno.tech>
2022-11-25 22:03:01 +0100

    condition home page

--- M1/Projets/react-calendar/app/src/pages/Home.jsx
+++ M1/Projets/react-calendar/app/src/pages/Home.jsx
@@ -1,10 +1,25 @@
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Calendar from "../components/Calendar";
-
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+	const currentUser = useSelector((state) => state.user);
+	const navigate = useNavigate();
 	return (
-		<Container>
-			<Calendar />
+		<Container sx={{ marginTop: 3 }}>
+			{currentUser.isLoggedIn ? (
+				<Calendar />
+			) : (
+				<Box>
+					<Typography variant="h4" color="initial">
+						Vous n'êtes pas connecté, veuillez créer un compte
+					</Typography>
+
+					<Button to="/login" variant="contained" sx={{ marginTop: 3 }} onClick={() => navigate("/login")}>
+						Login
+					</Button>
+				</Box>
+			)}
 		</Container>
 	);
 };
