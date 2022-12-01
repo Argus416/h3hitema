293dd94d1b1d9dfaf481cb2f416ba766d9621880
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2022-12-01 10:14:59 +0100

    vuejs, jour 4, les slots

---
+++ M1/VueJS/jour4/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import Menu from './components/Menu.vue'
+    import MyModal from './components/MyModal.vue'
+</script>
+
+<template>
+    <Menu />
+    <div class="container">
+
+        <MyModal>
+            <template #header>
+                <h1>toto</h1>
+            </template>
+            <template #body>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                </p>
+            </template>
+            <template #footer>
+                <button class="btn btn-danger" data-bs-toggle="modal">Fermer</button>
+            </template>
+        </MyModal>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ M1/VueJS/jour4/src/components/Menu.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="/">Jour 4</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a  class="nav-link "  href="/">Accueil</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link"  href="/">Contact</a>
+                    </li>
+
+                    <li  class="nav-item">
+                        <a class="nav-link"  href="/">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"  href="/">Login</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link"  href="/">Créer un compte</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
