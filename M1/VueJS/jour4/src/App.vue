b4b973eed0b93b9ece06b93d6cc74f972c454da4
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2022-12-01 10:34:41 +0100

    vuejs, jour 4, exo 1

--- M1/VueJS/jour4/src/App.vue
+++ M1/VueJS/jour4/src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
     import Menu from './components/Menu.vue'
     import MyModal from './components/MyModal.vue'
+
+    const allPays = [
+        {id: 0, nom : "Pays"},
+        {id: 1, nom : "France"},
+        {id: 2, nom : "Belgique"},
+        {id: 3, nom : "Allemagne"},
+    ]
 </script>
 
 <template>
@@ -9,16 +16,18 @@
 
         <MyModal>
             <template #header>
-                <h1>toto</h1>
+                <h1>Exo 1</h1>
             </template>
             <template #body>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                </p>
+                <form>
+                    <select class="form-select" aria-label="Default select example">
+                        <option  v-for="pays in allPays" key="pays.id">{{pays.nom}}</option>
+                    </select>
+                </form>
             </template>
             <template #footer>
                 <button class="btn btn-danger" data-bs-toggle="modal">Fermer</button>
+                <a href="https://google.com" target="_blank">google.com</a>
             </template>
         </MyModal>
     </div>
