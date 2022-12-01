c46eb3c23ab2f50d09f5fdccf2e2e258d6a289fe
Mohamad Al Khatib <mohamad@juno.tech>
2022-12-02 00:02:07 +0100

    vue Navbar

--- M1/Projets/facebook-clone/app/src/main.js
+++ M1/Projets/facebook-clone/app/src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "element-plus/dist/index.css";
+import "./style/style.scss";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(ElementPlus);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+	app.component(key, component);
+}
+
+app.mount("#app");
