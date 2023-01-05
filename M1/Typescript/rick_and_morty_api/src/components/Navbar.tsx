219774e78132f6ba29a3d1ad07319835a06e90e6
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2023-01-05 09:50:18 +0100

    add navbar btns

--- M1/Typescript/rick_and_morty_api/src/components/Navbar.tsx
+++ M1/Typescript/rick_and_morty_api/src/components/Navbar.tsx
@@ -7,6 +7,21 @@ const Navbar = () => {
 				<a className="navbar-brand" href="#">
 					<Logo />
 				</a>
+
+				<div className="collapse navbar-collapse my-nav" id="navbarSupportedContent">
+					<ul className="my-navbar-nav ms-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a className="my-nav-link" aria-current="page" href="https://rickandmortyapi.com/documentation">
+								Docs
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="my-nav-link my-secondary-btn" href="https://rickandmortyapi.com/support-us">
+								Support them
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
