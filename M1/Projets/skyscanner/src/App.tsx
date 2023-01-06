a7cf06dbc3728da480b2fb557594aa3e22520060
Mohamad Al Khatib <mohamad@juno.tech>
2023-01-06 01:55:36 +0100

    add navbar

--- M1/Projets/skyscanner/src/App.tsx
+++ M1/Projets/skyscanner/src/App.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import Home from "./pages/Home";
 import "./style/App.scss";
 import Navbar from "./components/Navbar";
