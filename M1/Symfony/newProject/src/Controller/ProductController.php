752c8d44b66fba0b5836d99d081859caec20b610
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2023-01-30 12:37:32 +0100

    add bootstrap, display products

--- M1/Symfony/newProject/src/Controller/ProductController.php
+++ M1/Symfony/newProject/src/Controller/ProductController.php
@@ -11,15 +11,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     private $products = [
-        1 => 'Pomme',
-        2 => 'Orange',
-        1 => 'Pomme de terre',
+        1 => [
+            'name' => 'Pomme',
+            'price' => 1.2,
+            'photo' => 'https://images.pexels.com/photos/206959/pexels-photo-206959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        ],
+        2 => [
+            'name' => 'Orange',
+            'price' => 3,
+            'photo' => 'https://images.pexels.com/photos/161559/background-bitter-breakfast-bright-161559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        ],
+        3 => [
+            'name' => 'Pomme de terre',
+            'price' => 5,
+            'photo' => 'https://images.pexels.com/photos/2286776/pexels-photo-2286776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        ],
     ];
     
-    #[Route('/product/{id}', name: 'app_product')]
-    public function index(string $id): Response
+    #[Route('/products', name: 'app_product')]
+    public function index(): Response
     {
         return $this->render('product/index.html.twig', [
+            'products' => $this->products,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_one_product')]
+    public function product(string $id): Response
+    {
+        return $this->render('product/product.html.twig', [
             'product' => $this->products[$id],
         ]);
     }
